6d818d57e4d457d7f9bcdcee8950157ee0b2532a
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-06-21 17:44:30 +0100

    Added contact page

--- js/about.js
+++ js/about.js
@@ -45,17 +45,19 @@ function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  if (slides.length && dots.length) {
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active-dot";
+    setTimeout(showSlides, 7000); // Change image every 7 seconds
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active-dot", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active-dot";
-  setTimeout(showSlides, 7000); // Change image every 7 seconds
 }
